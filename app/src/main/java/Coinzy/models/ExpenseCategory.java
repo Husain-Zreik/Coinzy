567950f9647e0f310a31b6c5a41248196b998a73
Husain-Zreik <husain.zreik1@gmail.com>
2024-09-07 12:34:17 +0300

    create expense category model

---
+++ app/src/main/java/Coinzy/models/ExpenseCategory.java
@@ -0,0 +1,32 @@
+package Coinzy.models;
+
+public class ExpenseCategory {
+    private int id;
+    private int userId;
+    private String categoryName;
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
